e69c01ff71541e6209e3ac829fb0f68fc146e7bb
P2wn <racecarboy77@gmail.com>
2026-09-13 09:57:00 -0400

    Harden messaging, channel reloads, and network delivery

--- src/main/java/dev/rosewood/rosechat/command/command/ReplyCommand.java
+++ src/main/java/dev/rosewood/rosechat/command/command/ReplyCommand.java
@@ -9,9 +9,6 @@ import dev.rosewood.rosegarden.command.framework.ArgumentsDefinition;
 import dev.rosewood.rosegarden.command.framework.CommandContext;
 import dev.rosewood.rosegarden.command.framework.CommandInfo;
 import dev.rosewood.rosegarden.command.framework.annotation.RoseExecutable;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ReplyCommand extends RoseChatCommand {
 
@@ -45,21 +42,6 @@ public class ReplyCommand extends RoseChatCommand {
             player.sendLocaleMessage("command-reply-no-one");
             return;
         }
-
-        OfflinePlayer target = Bukkit.getOfflinePlayer(targetName);
-
-        AtomicBoolean canBeMessaged = new AtomicBoolean(true);
-        if (!context.getSender().hasPermission("rosechat.togglemessage.bypass")) {
-            this.getAPI().getPlayerData(target.getUniqueId(), data -> {
-                if (data != null && !data.canBeMessaged()) {
-                    player.sendLocaleMessage("command-togglemessage-cannot-message");
-                    canBeMessaged.set(false);
-                }
-            });
-        }
-
-        if (!canBeMessaged.get())
-            return;
 
         MessageUtils.sendPrivateMessage(player, targetName, message);
     }
